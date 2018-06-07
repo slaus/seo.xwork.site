469e9e588a13011d75dee087d61a9edd82ca6bbb
Vyacheslav <mister.slaus@gmail.com>
2018-06-07 19:41:58 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -17,3 +17,120 @@ $('#back-to-top').click(function () {
     return false;
 });
 
+//Carousel responsive
+$(function () {
+    var jcarousel = $('.jcarousel');
+
+    jcarousel
+        .on('jcarousel:reload jcarousel:create', function () {
+            var carousel = $(this),
+                width = carousel.innerWidth();
+
+            if (width >= 991) {
+                width = width / 2;
+            } else if (width >= 767) {
+                width = width;
+            }
+
+            carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+        })
+        .jcarousel({
+            wrap: 'circular',
+            animation: {
+                duration: 1000,
+                speed: 1000,
+                easing: 'linear',
+                complete: function () {
+                }
+            }
+        })
+        .jcarouselAutoscroll({
+            interval: 10000,
+            target: '+=1',
+            autostart: true,
+        })
+        .on('mouseover', function (e) {
+            $(this).jcarouselAutoscroll('stop');
+        })
+        .on('mouseout', function (e) {
+            $(this).jcarouselAutoscroll('start');
+        });
+
+    var jcarousel = $('.jcarousel.work');
+
+    jcarousel
+        .on('jcarousel:reload jcarousel:create', function () {
+            var carousel = $(this),
+                width = carousel.innerWidth();
+            carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+        })
+        .jcarousel({
+            wrap: 'circular',
+            animation: {
+                duration: 1000,
+                speed: 1000,
+                easing: 'linear',
+                complete: function () {
+                }
+            }
+        })
+        .jcarouselAutoscroll({
+            interval: 50000,
+            target: '+=1',
+            autostart: true,
+        })
+        .on('mouseover', function (e) {
+            $(this).jcarouselAutoscroll('stop');
+        })
+        .on('mouseout', function (e) {
+            $(this).jcarouselAutoscroll('start');
+        });
+
+    $('.jcarousel-control-prev')
+        .jcarouselControl({
+            target: '-=1'
+        });
+
+    $('.jcarousel-control-next')
+        .jcarouselControl({
+            target: '+=1'
+        });
+
+    $('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function () {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function () {
+            $(this).removeClass('active');
+        })
+        .on('click', function (e) {
+            e.preventDefault();
+        })
+        .jcarouselPagination({
+            perPage: 1,
+            item: function (page) {
+                return '<a href="#' + page + '">' + page + '</a>';
+            }
+        });
+
+    $(function () {
+// Question handler
+        $('li.question').on('click', function () {
+
+            // gets next element
+            // opens answer of selected question
+            $(this).next().slideToggle(500)
+
+            // selects all other answers and slides up any open answer
+                .siblings('li.answer').slideUp();
+
+            //Grab img from clicked question
+            var caret = $(this).find('.caret');
+            //Remove Rotate class from all images except the active
+            $('.caret').not(caret).removeClass('rotate');
+            //toggle rotate class
+            caret.toggleClass('rotate');
+        });
+    });
+
+});
